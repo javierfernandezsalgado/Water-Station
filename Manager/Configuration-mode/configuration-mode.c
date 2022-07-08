0b35727fc0723385ab1ab047799b2796545f4899
javierfernandezsalgado <52181485+javierfernandezsalgado@users.noreply.github.com>
2022-07-08 18:02:18 +0200

    Configuration mode is added as first version

--- Manager/Configuration-mode/configuration-mode.c
+++ Manager/Configuration-mode/configuration-mode.c
@@ -3,6 +3,9 @@
 #include <Drivers/Screen/screen.h>
 #include <stdbool.h>
 
+
+static WiFiServer server;
+static String header;
 extern config_mode(void)
 {
   configure_wifi();
@@ -21,35 +24,52 @@ extern config_mode(void)
 static void website_configure()
 {
   WiFiClient client = server.available();
-
+    uint8_t c;
   uint8_t ssid_nominal = get_parameter(SSID_NOMINAL);
   uint8_t ssid_nominal_passwd = get_parameter(SSID_PASSW_NOMINAL);
 
   if (client)
-
+//see as example https://randomnerdtutorials.com/esp32-access-point-ap-web-server/
     {
-      //see as example https://randomnerdtutorials.com/esp32-access-point-ap-web-server/
+    log_info("New client");
+
+        while (client.connected()) {
+            if (client.available())
+            {
+                c = client.read();
+                header +=c;
+                log_info(c);
+                if (c == '\n') {
 
 
-      // In case that the user ends software restart
+                }
+                }
+
+
+                }
+
+
+
+
+                // In case that the user ends software restart
       ESP.restart();
     }
 }
 
 static void configure_wifi()
 {
-  uint16_t http_port = get_parameter(HTTP_PORT_CONFIGURE);
+    wifi_configuration * wifi_conf = (wifi_configuration * )get_parameter( parameter_bank.WIFI);
+    IPAddress ip;
+    uint16_t http_port = get_parameter(HTTP_PORT_CONFIGURE);
 
-  WiFiServer server(http_port);
-  const char* ssid     = get_parameter(SSID_CONFIGURATION);
-  const char* password = get_parameter(SSID_PASWD_CONFIGURATION);
+    server = WiFiServer(wifi_conf->http_port);
 
-  IPAddress ip;
-  set_parameter(IP_ADDRESS,ip);
-  print_screen(ip);
+    set_parameter(IP_ADDRESS,ip);
+    print_screen(ip);
 
-  log_info("Setting AP (Access Point)…");
-  WiFi.softAP(ssid, password);
-  IP=WiFi.softAPIP();
-  server.begin();
+    log_info("Setting AP (Access Point)…");
+    WiFi.softAP(wifi_conf->ssid_configuration, wifi_conf->ssid_passwd_configuration);
+    IP=WiFi.softAPIP();
+    server.begin();
+
 }
